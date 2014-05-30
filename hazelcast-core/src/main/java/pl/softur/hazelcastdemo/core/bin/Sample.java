2c653f1f3324d46189706d38e71f411fce131f45
Marcin Górski <mg@mgorski.net>
2014-05-30 19:37:02 +0200

    Hazelcast App ready for demo 31.05

--- hazelcast-core/src/main/java/pl/softur/hazelcastdemo/core/bin/Sample.java
+++ hazelcast-core/src/main/java/pl/softur/hazelcastdemo/core/bin/Sample.java
@@ -4,15 +4,7 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.InterfacesConfig;
 import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.IMap;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Marcin
- * Date: 27.05.14
- * Time: 23:28
- * To change this template use File | Settings | File Templates.
- */
 public class Sample {
 
     public static void main(String[] args) {
@@ -29,9 +21,5 @@ public class Sample {
         config.setNetworkConfig(networkConfig);
 
         Hazelcast.getOrCreateHazelcastInstance(config);
-
-
-        IMap
-
     }
 }
